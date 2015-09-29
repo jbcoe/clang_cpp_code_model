e6762a6115b6c8b95f194bbc1a2d601e3754e538
Jonathan Coe <jon@jbcoe.net>
2015-09-29 21:09:05 +0100

    simple tests for classes

--- tests/test_classes.py
+++ tests/test_classes.py
@@ -1,9 +1,15 @@
 import unittest
+import model
 
 class TestClasses(unittest.TestCase):
-    def testPasses(self):
-        pass
 
-    def testFails(self):
-        self.assertTrue(False)
+    def setUp(self):
+        classes = model.parse_classes('test_file.cpp')
+        self.class_a = classes[0]
 
+    def test_className(self):
+        assert(self.class_a.name == 'A')
+
+    def test_classMethods(self):
+        assert( len(self.class_a.functions) == 2)
+        
