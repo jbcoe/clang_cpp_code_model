090fdde48cf694bae9262f6292dbfa6f6cd0b927
Jonathan Coe <jon@jbcoe.net>
2015-12-06 15:23:38 +0000

    Tests written inline. Remove duplicate tests.

--- tests/test_classes.py
+++ tests/test_classes.py
@@ -4,37 +4,52 @@ from util import get_tu
 
 class TestClasses(unittest.TestCase):
 
-    def setUp(self):
-        source = """
-class A {
-  virtual int foo(int i, const char* p) const { return 0; }
-  void bar() { }
-  virtual int fubar() = 0;
-};"""   
-        tu = get_tu(source, 'cpp')
-        classes = model.build_classes(tu.cursor)
-        self.class_a = classes[0]
-
     def test_className(self):
-        assert(self.class_a.name == 'A')
+        source = 'class A{};'
+        tu = get_tu(source, 'cpp')
+
+        classes = model.build_classes(tu.cursor)
+
+        assert(len(classes)==1)
+        assert(classes[0].name == 'A')
 
     def test_classMethods(self):
-        assert( len(self.class_a.methods) == 3)
-        
+        source = """
+        class A{};
+        class B{
+            void foo();
+            int bar();
+        };"""
+        tu = get_tu(source, 'cpp')
+
+        classes = model.build_classes(tu.cursor)
+
+        assert(len(classes[0].methods) == 0)
+        assert(len(classes[1].methods) == 2)
+
     def test_classMethodReturnTypes(self):
-        assert( self.class_a.methods[0].return_type == "int")
-        assert( self.class_a.methods[1].return_type == "void")
-    
+        source = """
+        class B{
+            void foo();
+            int bar();
+        };"""
+        tu = get_tu(source, 'cpp')
+
+        classes = model.build_classes(tu.cursor)
+
+        assert(classes[0].methods[0].return_type == "void")
+        assert(classes[0].methods[1].return_type == "int")
+
     def test_classMethodArgumentTypes(self):
-        args = self.class_a.methods[0].arguments
-        assert( len(self.class_a.methods) == 2)
-        
-    def test_classMethodReturnTypes(self):
-        assert( self.class_a.methods[0].return_type == "int")
-        assert( self.class_a.methods[1].return_type == "void")
-    
-    def test_classMethodArgumentTypes(self):
-        args = self.class_a.methods[0].arguments
+        source = """
+        class A {
+            int foo(int i, const char* p);
+        };"""
+        tu = get_tu(source, 'cpp')
+
+        classes = model.build_classes(tu.cursor)
+        args = classes[0].methods[0].arguments
+
         assert(args[0].type == "int")
         assert(args[0].name == "i")
         assert(args[1].type == "const char *") 
@@ -42,26 +57,49 @@ class A {
         assert(args[1].name == "p")
 
     def test_classMethodConstQualifiers(self):
-        assert( self.class_a.methods[0].is_const == True)
-        assert( self.class_a.methods[1].is_const == False)
-    
+        source = """
+        class A {
+            int foo() const;
+            int bar();
+        };"""
+        tu = get_tu(source, 'cpp')
+
+        classes = model.build_classes(tu.cursor)
+        methods = classes[0].methods
+
+        assert( methods[0].is_const == True)
+        assert( methods[1].is_const == False)
+
     def test_classMethodsAreVirtual(self):
-        assert( self.class_a.methods[0].is_virtual == True)
-        assert( self.class_a.methods[0].is_pure_virtual == False)
-        assert( self.class_a.methods[1].is_virtual == False)
-        assert( self.class_a.methods[2].is_pure_virtual == True)
+        source = """
+        class A {
+            virtual int foo();
+            int bar();
+            virtual int foobar() = 0;
+        };"""
+        tu = get_tu(source, 'cpp')
+
+        classes = model.build_classes(tu.cursor)
+        methods = classes[0].methods
+
+        assert( methods[0].is_virtual == True)
+        assert( methods[0].is_pure_virtual == False)
+        assert( methods[1].is_virtual == False)
+        assert( methods[2].is_pure_virtual == True)
 
     def test_namespaces(self):
         source = """
-class A{};
-namespace outer {
-    class B{};
-    namespace inner {
-        class C{};
-    } // end inner
-} // end outer"""
+        class A{};
+        namespace outer {
+            class B{};
+            namespace inner {
+                class C{};
+            } // end inner
+        } // end outer"""
         tu = get_tu(source, 'cpp')
+
         classes = model.build_classes(tu.cursor)
+
         assert(classes[0].namespace == "");
         assert(classes[1].namespace == "outer");
         assert(classes[2].namespace == "outer::inner");
